39d43f00c5addc1d14ff8bccd8843899568e9651
devopsodia <odiadevops@gmail.com>
2025-09-26 04:46:34 +0530

    added frontend code for forgot password

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -34,7 +34,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <h1>Dashboard</h1>
+        <h1><span className="grep">Grep</span><span className="mind">Mind</span></h1>
         <div className="header-right">
           <span className="user-name">Welcome, {user?.name || 'User'}</span>
           <button onClick={handleLogout} className="logout-button">
